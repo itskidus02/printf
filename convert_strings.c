3073a1e2000be64b31a33be0ec5c010e247682e7
Kidus Asebe <138166404+itskidus02@users.noreply.github.com>
2023-12-24 22:18:09 +0300

    Update convert_strings.c

--- convert_strings.c
+++ convert_strings.c
@@ -1,5 +1,6 @@
 #include "main.h"
 
+
 unsigned int convert_s(va_list args, buffer_t *output,
 		unsigned char flags, int wid, int prec, unsigned char len);
 unsigned int convert_S(va_list args, buffer_t *output,
